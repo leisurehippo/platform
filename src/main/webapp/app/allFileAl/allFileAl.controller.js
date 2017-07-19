25c8db04659a76d84d41f5bd87eafff79c528f5b
buzhiningque <835373492@qq.com>
2017-07-19 15:34:49 +0800

    'fix'

---
+++ src/main/webapp/app/allFileAl/allFileAl.controller.js
@@ -0,0 +1,54 @@
+/**
+ * Created by gsy on 2016/11/8.
+ */
+angular
+    .module('jhipsterSampleApplicationApp')
+    .controller('AllFileAlController',AllFileAlController);
+
+AllFileAlController.$inject = ['$scope', '$http', '$state', 'GetAllData', 'GetAlgorithmData', 'HdfsUpload'];
+function AllFileAlController($scope, $http, $state, GetAllData, GetAlgorithmData, HdfsUpload) {
+    var vm = this;
+    vm.fileData = [];
+    vm.algrithmData = [];
+    GetAllData.get({}, function (result) {
+        for (var i = 0; i< result.length; i++) {
+            vm.fileData[i] = result[i].split("+");
+            if (vm.fileData[i][1] == '0') {
+                vm.fileData[i][1] = false;
+            }else
+                vm.fileData[i][1] = true;
+
+        }
+        console.log(vm.fileData);
+    }, function (result) {
+    });
+
+    var type="Algorithm";
+    GetAlgorithmData.get({Type:type},function (res) {
+        console.log(res);
+        vm.algrithmData = res;
+    }, function (result) {
+    });
+
+    vm.alFileUpload = alFileUpload;
+    function alFileUpload() {
+        $state.go('fileUpload', {fileType:1});
+    }
+
+    vm.dataFileUpload = dataFileUpload;
+    function dataFileUpload() {
+        $state.go('fileUpload',  {fileType:0});
+    }
+
+    vm.fileUpHdfs = fileUpHdfs;
+    function fileUpHdfs(index) {
+        console.log(index);
+        HdfsUpload.get({DataName:vm.fileData[index][0]}, function (res) {
+            console.log(res);
+            $state.go('allFileData', null, { reload: true });
+        }, function (res) {
+            console.log(res);
+        });
+    }
+
+}
