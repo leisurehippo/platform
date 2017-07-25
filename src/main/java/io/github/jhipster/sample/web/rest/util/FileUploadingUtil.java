853916fe870485728b996152d914573b0bf98777
leisurehippo <974292414@qq.com>
2017-07-25 20:04:29 +0800

    modify utf-8

--- src/main/java/io/github/jhipster/sample/web/rest/util/FileUploadingUtil.java
+++ src/main/java/io/github/jhipster/sample/web/rest/util/FileUploadingUtil.java
@@ -117,58 +117,70 @@ public class FileUploadingUtil {
      * @throws IOException
      */
     public static boolean writeFileContent(String filepath,String newstr) throws IOException{
-        Boolean bool = false;
-        String filein = newstr+"\r\n";//新写入的行，换行
-        String temp  = "";
+//        Boolean bool = false;
+//        String filein = newstr+"\r\n";//新写入的行，换行
+//        String temp  = "";
+//
+//        FileInputStream fis = null;
+//        InputStreamReader isr = null;
+//        BufferedReader br = null;
+//        FileOutputStream fos  = null;
+//        PrintWriter pw = null;
+//        try {
+//            File file = new File(filepath);//文件路径(包括文件名称)
+//            //将文件读入输入流
+//            fis = new FileInputStream(file);
+//            isr = new InputStreamReader(fis);
+//            br = new BufferedReader(isr);
+//            StringBuffer buffer = new StringBuffer();
+//
+//            //文件原有内容
+//            for(int i=0;(temp =br.readLine())!=null;i++){
+//                buffer.append(temp);
+//                // 行与行之间的分隔符 相当于“\n”
+//                buffer = buffer.append(System.getProperty("line.separator"));
+//            }
+//            buffer.append(filein);
+//            fos = new FileOutputStream(file);
+//            pw = new PrintWriter(fos);
+//            pw.write(buffer.toString().toCharArray());
+//            pw.flush();
+//            bool = true;
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//            e.printStackTrace();
+//        }finally {
+//            //不要忘记关闭
+//            if (pw != null) {
+//                pw.close();
+//            }
+//            if (fos != null) {
+//                fos.close();
+//            }
+//            if (br != null) {
+//                br.close();
+//            }
+//            if (isr != null) {
+//                isr.close();
+//            }
+//            if (fis != null) {
+//                fis.close();
+//            }
+//        }
+//        return bool;
 
-        FileInputStream fis = null;
-        InputStreamReader isr = null;
-        BufferedReader br = null;
-        FileOutputStream fos  = null;
-        PrintWriter pw = null;
-        try {
-            File file = new File(filepath);//文件路径(包括文件名称)
-            //将文件读入输入流
-            fis = new FileInputStream(file);
-            isr = new InputStreamReader(fis);
-            br = new BufferedReader(isr);
-            StringBuffer buffer = new StringBuffer();
-
-            //文件原有内容
-            for(int i=0;(temp =br.readLine())!=null;i++){
-                buffer.append(temp);
-                // 行与行之间的分隔符 相当于“\n”
-                buffer = buffer.append(System.getProperty("line.separator"));
-            }
-            buffer.append(filein);
-
-            fos = new FileOutputStream(file);
-            pw = new PrintWriter(fos);
-            pw.write(buffer.toString().toCharArray());
-            pw.flush();
-            bool = true;
-        } catch (Exception e) {
-            // TODO: handle exception
+        try{
+            File file = new File(filepath);
+            OutputStream out=new FileOutputStream(file);
+            BufferedWriter rd = new BufferedWriter(new OutputStreamWriter(out,"utf-8"));
+            rd.write(newstr);
+            rd.close();
+            out.close();
+            return true;
+        }catch(IOException e){
             e.printStackTrace();
-        }finally {
-            //不要忘记关闭
-            if (pw != null) {
-                pw.close();
-            }
-            if (fos != null) {
-                fos.close();
-            }
-            if (br != null) {
-                br.close();
-            }
-            if (isr != null) {
-                isr.close();
-            }
-            if (fis != null) {
-                fis.close();
-            }
+            return false;
         }
-        return bool;
     }
 
     /**
