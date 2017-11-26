b17d81a46fc4d1ad3a24da72276e928b996ee30c
sun <973945379@qq.com>
2017-11-26 22:55:49 +0800

    update dataX url

--- src/main/java/io/github/jhipster/sample/config/ETLConfig.java
+++ src/main/java/io/github/jhipster/sample/config/ETLConfig.java
@@ -2,5 +2,5 @@ package io.github.jhipster.sample.config;
 
 public class ETLConfig {
 	
-	public static final String DATAXURL = "C:/代码相关/平台/datax";
+	public static final String DATAXURL = "/home/hadoop/platform/datax";
 }
